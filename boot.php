84b89f2b735f75296a133d6b7b97e8ee7de3bb55
Thomas Skerbis <thomas.skerbis@mac.com>
2021-09-25 14:36:59 +0200

    Update boot.php

--- boot.php
+++ boot.php
@@ -1,22 +1,23 @@
 <?php
-
-
-
-
 if (rex::isBackend()) {
     $addon = rex_addon::get('slice_columns');
-	
-	rex_view::setJsProperty('slicesteps', (int)$addon->getConfig('number_steps'));
+
+    rex_view::setJsProperty('slicesteps', (int)$addon->getConfig('number_steps'));
     rex_view::setJsProperty('min_width_column', (int)$addon->getConfig('min_width_column'));
     rex_view::setJsProperty('number_columns', (int)$addon->getConfig('number_columns'));
-    
-	rex_view::addCssFile($addon->getAssetsUrl('columns.css'));
+
+    rex_view::addCssFile($addon->getAssetsUrl('columns.css'));
 
     rex_view::addJsFile($addon->getAssetsUrl('sortable.min.js'));
     rex_view::addJsFile($addon->getAssetsUrl('columns_sortablejs.js'));
 
-    // add buttons to slice menu
-    rex_extension::register('SLICE_MENU', ['columns', 'addButtons']);
+    // templates ausschließen	
+    $templates = explode("|", $addon->getConfig('templates'));
+    if (in_array(rex_article::getCurrent()->getTemplateId(), $templates)) {
+    } else {
+        // add buttons to slice menu
+        rex_extension::register('SLICE_MENU', ['columns', 'addButtons']);
+    }
 
     rex_extension::register('SLICE_SHOW', array('columns', 'show'));
 } else {
@@ -25,5 +26,4 @@ if (rex::isBackend()) {
     // rex_extension::register('STRUCTURE_CONTENT_AFTER_SLICES', array('columns', 'frontend'));
     // rex_extension::register('SLICE_OUTPUT', array('columns', 'frontend'));
     rex_extension::register('SLICE_SHOW', array('columns', 'frontend'), rex_extension::LATE);
-
 }
