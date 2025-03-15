3be1584533c0dc33efb7bb7f4214d2f751abcc35
Thomas Skerbis <thomas.skerbis@mac.com>
2025-03-15 18:55:28 +0100

    Update api_section.php

--- lib/api_section.php
+++ lib/api_section.php
@@ -12,7 +12,23 @@ class rex_api_section extends rex_api_function
         $clangId = rex_request('clang', 'int', 1);
         $sectionId = rex_request('section_id', 'int', 0);
         
-        if ($function === 'add_to_section') {
+        if ($function === 'get_sections') {
+            // Hole alle Sections für diesen Artikel
+            $sql = rex_sql::factory();
+            $sql->setQuery('SELECT DISTINCT section_id FROM ' . rex::getTablePrefix() . 'article_slice 
+                            WHERE article_id = :article_id AND clang_id = :clang_id AND section_id IS NOT NULL AND section_id > 0', 
+                          ['article_id' => $articleId, 'clang_id' => $clangId]);
+            
+            $sections = [];
+            for ($i = 0; $i < $sql->getRows(); $i++) {
+                $sections[$sql->getValue('section_id')] = 'Section ' . $sql->getValue('section_id');
+                $sql->next();
+            }
+            
+            echo json_encode($sections);
+            exit;
+        }
+        elseif ($function === 'add_to_section') {
             // Wenn keine section_id übergeben wurde, erstelle eine neue
             if ($sectionId === 0) {
                 $sql = rex_sql::factory();
@@ -49,6 +65,35 @@ class rex_api_section extends rex_api_function
             }
             
             echo json_encode(['status' => 'success', 'message' => 'Slice aus Section entfernt']);
+            exit;
+        }
+        elseif ($function === 'get_section_settings') {
+            $sectionId = rex_request('section_id', 'int', 0);
+            
+            $addon = rex_addon::get('slice_columns');
+            $sectionSettings = $addon->getConfig('section_settings', []);
+            
+            $settings = isset($sectionSettings[$sectionId]) ? $sectionSettings[$sectionId] : [];
+            
+            echo json_encode($settings);
+            exit;
+        }
+        elseif ($function === 'save_section_settings') {
+            $sectionId = rex_request('section_id', 'int', 0);
+            $sectionClass = rex_request('section_class', 'string', '');
+            $sectionBackground = rex_request('section_background', 'string', '');
+            
+            $addon = rex_addon::get('slice_columns');
+            $sectionSettings = $addon->getConfig('section_settings', []);
+            
+            $sectionSettings[$sectionId] = [
+                'class' => $sectionClass,
+                'background' => $sectionBackground
+            ];
+            
+            $addon->setConfig('section_settings', $sectionSettings);
+            
+            echo json_encode(['status' => 'success', 'message' => 'Section-Einstellungen gespeichert']);
             exit;
         }
         
