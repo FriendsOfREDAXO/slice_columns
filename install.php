8d8be4c21573a1c91174aeea16b60c3e8de1c123
Thomas Skerbis <thomas.skerbis@mac.com>
2025-03-15 18:44:56 +0100

    Update install.php

--- install.php
+++ install.php
@@ -1,5 +1,13 @@
 <?php
 
+<?php
+
+// Bestehende Funktionalität beibehalten
 rex_sql_table::get(rex::getTablePrefix() . 'article_slice')
     ->ensureColumn(new rex_sql_column('slice_size', 'varchar(255)'))
+    ->ensure();
+
+// Neue Spalte für Sections hinzufügen
+rex_sql_table::get(rex::getTablePrefix() . 'article_slice')
+    ->ensureColumn(new rex_sql_column('section_id', 'int(10)', true))
     ->ensure();
